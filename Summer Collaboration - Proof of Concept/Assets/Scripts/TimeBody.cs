820c4978ea11cf3b70f8f424c9caa8eace2db514
michaelmich3 <michaelmich3@hotmail.com>
2019-06-10 00:53:09 -0500

    Initial Commit

---
+++ Summer Collaboration - Proof of Concept/Assets/Scripts/TimeBody.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeBody : MonoBehaviour
+{
+    [SerializeField] private float recordTime = 10f;
+
+    private bool isRewinding = false;
+    List<PointInTime> pointsInTime;
+    Rigidbody rb;
+
+    void Start()
+    {
+        pointsInTime = new List<PointInTime>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            StartRewind();
+        }
+        if (Input.GetMouseButtonUp(1))
+        {
+            StopRewind();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (isRewinding)
+        {
+            Rewind();
+        }
+        else
+        {
+            Record();
+        }
+    }
+
+    void Rewind()
+    {
+        if (pointsInTime.Count > 0)
+        {
+            PointInTime pointInTime = pointsInTime[0];
+            transform.position = pointInTime.position;
+            transform.rotation = pointInTime.rotation;
+            pointsInTime.RemoveAt(0);
+        }
+        else
+        {
+            //StopRewind();
+        }
+    }
+
+    void Record()
+    {
+        if (pointsInTime.Count > Mathf.Round(recordTime / Time.fixedDeltaTime))
+        {
+            pointsInTime.RemoveAt(pointsInTime.Count - 1);
+        }
+
+        pointsInTime.Insert(0, new PointInTime(transform.position, transform.rotation));
+    }
+
+    public void StartRewind()
+    {
+        isRewinding = true;
+        rb.isKinematic = true;
+    }
+
+    public void StopRewind()
+    {
+        isRewinding = false;
+        rb.isKinematic = false;
+    }
+}
