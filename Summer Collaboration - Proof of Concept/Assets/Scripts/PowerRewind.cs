0e30fff681b8b85c74dcd75fdd554081b9933dd1
michaelmich3 <michaelmich3@hotmail.com>
2019-06-11 05:04:18 -0500

    Updated a bunch of stuff: Time rewind code Added Hands and watch models Added round UI bar on watch Added animations for hands and watch for time rewind And more smaller things

--- Summer Collaboration - Proof of Concept/Assets/Scripts/PowerRewind.cs
+++ Summer Collaboration - Proof of Concept/Assets/Scripts/PowerRewind.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class PowerRewind : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Tooltip("How far back in time the object can be reversed (in seconds). Negative value for infinite time.")]
+    [SerializeField] private float rewindTime = 10f;
+    [SerializeField] private GameObject hands;
+    [SerializeField] private GameObject watch;
+
+    private bool isRewinding = false;
+
+    private void Start()
     {
-        
+        Animator anim = watch.GetComponent<Animator>();
+        AnimatorStateInfo state = anim.GetCurrentAnimatorStateInfo(0);
+        anim.Play(state.fullPathHash, -1, Random.Range(0f, 1f));
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(1))
+        {
+            StartRewind();
+        }
+        if (Input.GetMouseButtonUp(1))
+        {
+            StopRewind();
+        }
+    }
+
+    public void StartRewind()
+    {
+        isRewinding = true;
+        hands.GetComponent<Animator>().SetBool("IsRewinding", true);
+        watch.GetComponent<Animator>().SetFloat("WatchRewind", -1);
+    }
+
+    public void StopRewind()
+    {
+        isRewinding = false;
+        hands.GetComponent<Animator>().SetBool("IsRewinding", false);
+        watch.GetComponent<Animator>().SetFloat("WatchRewind", 1);
     }
 }
