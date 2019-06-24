411dcbc70f0fddaf9587c627cb984da4bb96e23e
Caleb Flick <calebflick@gmail.com>
2019-06-24 10:58:29 -0500

    added character controller and camera controller

---
+++ Summer Collaboration Project/Assets/Scripts/CameraController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//This script goes on the Player GameObject
+public class CameraController : MonoBehaviour
+{
+    #region Variables
+
+    [SerializeField]
+    private float minVerticalLookAngle = -60.0f;        //x axis min clamp
+    [SerializeField]
+    private float maxVerticalLookAngle = 60.0f;         //x axis max clamp
+
+    [SerializeField]
+    private float verticalLookSensitivity = 5.0f;      //x axis sensitivity
+    [SerializeField]
+    private float horizontalLookSensitivity = 3.0f;    //y axis sensitivity
+
+    private Camera _firstPersonCamera;
+
+    private float _currentYRotation;
+    private float _currentXRotation;
+
+    private const string CANCELBUTTONNAME = "Cancel";
+
+    #endregion
+
+    private void Awake()
+    {
+        _currentYRotation = 0.0f;
+        _currentXRotation = 0.0f;
+
+        _firstPersonCamera = this.gameObject.GetComponentInChildren<Camera>();      //TODO: add check for whether there is actually a camera in children
+
+        LockCursor();
+    }
+    
+    private void Update()
+    {
+        GetMouseInput();
+        RotatePlayerAndCamera();
+
+        /* Unlock cursor */
+        if (Input.GetButton(CANCELBUTTONNAME))
+        {
+            UnlockCursor();
+        }
+    }
+
+    private void GetMouseInput()
+    {
+        /* Get mouse input */
+        _currentYRotation += Input.GetAxis("Mouse X") * verticalLookSensitivity;
+        _currentXRotation += Input.GetAxis("Mouse Y") * horizontalLookSensitivity;
+
+        /* Clamp vertical look angle */
+        _currentXRotation = Mathf.Clamp(_currentXRotation, minVerticalLookAngle, maxVerticalLookAngle);
+    }
+
+    private void RotatePlayerAndCamera()
+    {
+        /* Rotate player and camera based on mouse input */
+        this.gameObject.transform.localEulerAngles = new Vector3(0, _currentYRotation, 0);
+        _firstPersonCamera.transform.localEulerAngles = new Vector3(_currentXRotation, _currentYRotation, 0);   //needs to be -_currentXRotation if MouseY is not set to inverse in project settings
+    }
+
+    //TODO: move these to UI script later on
+    private void LockCursor()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void UnlockCursor()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+}
---
+++ Summer Collaboration Project/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+
+//This script goes on the Player GameObject
+public class CharacterController : MonoBehaviour
+{
+    #region Variables
+
+    [SerializeField]
+    private int walkSpeed = 250;
+
+    private Rigidbody _rb;
+    private Vector3 _movementDirection;
+
+    #endregion
+
+    private void Awake()
+    {
+        _rb = this.gameObject.GetComponent<Rigidbody>();
+    }
+    
+    private void Update()
+    {
+        GetMovementInput();
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    private void GetMovementInput()
+    {
+        /* Get directional input */
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
+
+        /* Create vector 3 for x and z velocity */
+        _movementDirection = (horizontalInput * transform.right + verticalInput * transform.forward).normalized;
+    }
+
+    private void MovePlayer()
+    {
+        /* Get current y velocity */
+        Vector3 yVelocity = new Vector3(0, _rb.velocity.y, 0);
+
+        /* Move player based on velocity from directional input */
+        _rb.velocity = _movementDirection * walkSpeed * Time.deltaTime;
+
+        /* Add y velocity back in */
+        _rb.velocity += yVelocity;      //since _movementDirection only has values for x and z velocity we need to add y velocity to not constantly set vertical movement to 0 every fixed update
+    }
+}
