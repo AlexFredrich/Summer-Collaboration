1c4e82087d82d79252d83dc0e80fb28fb05c7651
apod1010 <32077837+apod1010@users.noreply.github.com>
2019-07-02 13:19:46 -0500

    UI Mock Main Menu

---
+++ Summer Collaboration Project/Assets/UI/UI Scripts/WatchFaceScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WatchFaceScript : MonoBehaviour
+{
+    public GameObject watchPivot;
+
+
+    public void WatchPointNorth()
+    {
+        watchPivot.transform.rotation = Quaternion.Euler(0, 0, 0);
+    }
+
+    public void WatchPointEast()
+    {
+        watchPivot.transform.rotation = Quaternion.Euler(0, 90, 0);
+    }
+
+    public void WatchPointSouth()
+    {
+        watchPivot.transform.rotation = Quaternion.Euler(0, 180, 0);
+    }
+
+    public void WatchPointWest()
+    {
+        watchPivot.transform.rotation = Quaternion.Euler(0, 270, 0);
+    }
+}
