fdad997277d1ad7c08cf0e762dd7b91413572ad0
BrendanWascher <brendanwascher@hotmail.com>
2019-10-13 18:56:58 -0500

    Brendan Script Update
    Duplicates scripts from script folder into brendan's folder (different names)
    Adds states to drawbridge
    Adds event to drawbridge

---
+++ Summer Collaboration Project/Assets/BrendanFolder/BrendanScript/DrawbridgeClearenceCheck.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class DrawbridgeClearenceCheck : MonoBehaviour
+{
+    public UnityEvent bridgeInterference;
+
+    [SerializeField]
+    private Transform triggerCheck;
+
+    public bool isBlocked = true;
+    public enum bridgeState { lowered, lowering, raising, raised}
+    public bridgeState currentState;
+
+    private int blockerCount = 0;
+
+    private void Start()
+    {
+        if(bridgeInterference == null)
+        {
+            bridgeInterference = new UnityEvent();
+        }
+        currentState = bridgeState.raised;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag != "Player")
+        {
+            if(currentState != bridgeState.raised || currentState != bridgeState.raising)
+            {
+                bridgeInterference.Invoke();
+                isBlocked = true;
+            }        
+            blockerCount++;
+            //change state if needed
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag != "Player")
+        {
+            blockerCount--;
+            if (blockerCount == 0)
+            {
+                isBlocked = false;
+            }
+            //change state
+        }
+    }
+}
---
+++ Summer Collaboration Project/Assets/BrendanFolder/BrendanScript/LowerDrawbridge.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class LowerDrawbridge : MonoBehaviour
+{
+    [SerializeField]
+    private Transform BridgeOne;
+    [SerializeField]
+    private Transform BridgeTwo;
+    [SerializeField]
+    private DrawbridgeClearenceCheck drawBridgeChecker;
+
+    public void Activate()
+    {
+        if (!drawBridgeChecker.isBlocked)
+        {
+            //lower drawbridges
+        }
+        else
+        {
+            //feedback to indicate blockage
+        }
+    }
+
+    public void InterferenceDetected()
+    {
+        //check if raising is needed
+        //change state if needed
+        //raise drawbridges at 2x speed
+        //lights flash?
+        //sound plays?
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnEnable()
+    {
+        drawBridgeChecker.bridgeInterference.AddListener(InterferenceDetected);
+    }
+    private void OnDisable()
+    {
+        drawBridgeChecker.bridgeInterference.RemoveListener(InterferenceDetected);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void RaiseBridge()
+    {
+        drawBridgeChecker.currentState = DrawbridgeClearenceCheck.bridgeState.raising;
+    }
+
+    void LowerBridge()
+    {
+        drawBridgeChecker.currentState = DrawbridgeClearenceCheck.bridgeState.lowering;
+    }
+
+
+}
